f74a741455179426824c73fc18f2f635149ac49a
Kreijstal <rainb@tfwno.gf>
2024-08-09 22:09:30 +0200

    not really good

--- openvaf/llvm/wrapper/OpenVafWrapper.cpp
+++ openvaf/llvm/wrapper/OpenVafWrapper.cpp
@@ -1,8 +1,11 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/CrashRecoveryContext.h"
-#include <llvm/IR/Attributes.h>
-#include <llvm/IR/Function.h>
-#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Transforms/Vectorize/SLPVectorizer.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 #include <iostream>
 #include <mutex>
@@ -12,22 +15,15 @@ using namespace llvm;
 
 extern "C" {
 
-// Enable some fast-math flags for an operation
-// These flags are used for derivatives by default because they only change
-// the rounding behaviour which is not relevant for automatically generated code
-// (derivatives in OpenVAF)
-//
-// https://llvm.org/docs/LangRef.html#fast-math-flags
 void LLVMSetPartialFastMath(LLVMValueRef V) {
   if (auto I = dyn_cast<Instruction>(unwrap<Value>(V))) {
+    I->setFast(true);
     I->setHasAllowReassoc(true);
     I->setHasAllowReciprocal(true);
     I->setHasAllowContract(true);
   }
 }
 
-// Enable fast-math flags for an operation
-// https://llvm.org/docs/LangRef.html#fast-math-flags
 void LLVMSetFastMath(LLVMValueRef V) {
   if (auto I = dyn_cast<Instruction>(unwrap<Value>(V))) {
     I->setFast(true);
@@ -40,8 +36,39 @@ void LLVMPurgeAttrs(LLVMValueRef V) {
   }
 }
 
+// Keep this function for backwards compatibility
 void LLVMPassManagerBuilderSLPVectorize(LLVMPassManagerBuilderRef PMB) {
   PassManagerBuilder *Builder = unwrap(PMB);
   Builder->SLPVectorize = true;
 }
+
+// New function to add SLPVectorizer pass using the new pass manager
+void addSLPVectorizerPass(ModulePassManager &MPM) {
+  FunctionPassManager FPM;
+  FPM.addPass(SLPVectorizerPass());
+  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 }
+
+// New function to set up the optimization pipeline using the new pass manager
+void setupOptimizationPipeline(Module &M) {
+  LoopAnalysisManager LAM;
+  FunctionAnalysisManager FAM;
+  CGSCCAnalysisManager CGAM;
+  ModuleAnalysisManager MAM;
+
+  PassBuilder PB;
+
+  PB.registerModuleAnalyses(MAM);
+  PB.registerCGSCCAnalyses(CGAM);
+  PB.registerFunctionAnalyses(FAM);
+  PB.registerLoopAnalyses(LAM);
+  PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
+
+  ModulePassManager MPM = PB.buildPerModuleDefaultPipeline(OptimizationLevel::O2);
+
+  addSLPVectorizerPass(MPM);
+
+  MPM.run(M, MAM);
+}
+
+} // extern "C"
